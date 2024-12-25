7dae9f7fff15bb7b2b0043ad731f89407750ed1f
KAUSHIK1205 <chezhiworld@gmail.com>
2024-12-25 17:23:42 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,9 @@
+dic1=["india","brazil"]
+dic2=["cricket","football"]
+dic3={k:v for k,v in  zip(dic1,dic2)}
+print(dic3)
+
+di={1,2,3,4,5}
+dic={5,4,3,2,1}
+for a,b in  zip  (di,dic):
+    print(a,b)
